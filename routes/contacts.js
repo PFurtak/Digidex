7c22b3192065598fbc60928f3d6fdd072ab48a98
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-05 22:07:24 -0400

    configured post route for contacts, confirmed posting to database, confirmed get contacts request is tied to user JWT

--- routes/contacts.js
+++ routes/contacts.js
@@ -23,9 +23,31 @@ router.get('/', auth, async (req, res) => {
 // POST route for api/contacts
 // Add contact
 // Private access
-router.post('/', (req, res) => {
-  res.send('add contact');
-});
+router.post(
+  '/',
+  [auth, [check('name', 'Name is required.').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ erros: errors.array() });
+    }
+    const { name, email, phone, type } = req.body;
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 // PUT route for api/contacts/:id
 // update contact
