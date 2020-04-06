aa9a1e03216c8776ef461708893a7df98e628f4c
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-06 13:34:26 -0400

    set up global state management for Contact components, defined types

---
+++ client/src/context/contact/ContactState.jsx
@@ -0,0 +1,67 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from '../types';
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Patrick Furtak',
+        email: 'pfurtak@gmail.com',
+        phone: '444-444-4444',
+        type: 'professional',
+      },
+      {
+        id: 2,
+        name: 'Bill Burr',
+        email: 'bburr@gmail.com',
+        phone: '555-555-5555',
+        type: 'professional',
+      },
+      {
+        id: 3,
+        name: 'Tom Segura',
+        email: 'tommybunz@gmail.com',
+        phone: '666-666-6666',
+        type: 'personal',
+      },
+    ],
+  };
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Add Contact
+
+  // Delete contact
+
+  // Set current contact
+
+  // Clear current contact
+
+  // Update contact
+
+  // Filter contacts
+
+  // Clear filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+      }}>
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
