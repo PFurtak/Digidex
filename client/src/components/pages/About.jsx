6144db8c9bb0b3190f5abcef90c91919bb1a074a
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-06 00:54:50 -0400

    installed dependencies, cleaned up CRA boiler, brought in css and fontawsm, created navbar, home and about pages. brought in react router

---
+++ client/src/components/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this application</h1>
+      <p className='my-1'>Digital roladex for managing your contacts.</p>
+      <p className='bg-dark p'>
+        <strong>Version: 0.0.1</strong>
+      </p>
+    </div>
+  );
+};
+
+export default About;
