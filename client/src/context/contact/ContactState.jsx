c5209d2536d9f903f8e9c4ca897a2e050019b6ef
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-06 23:17:25 -0400

    brought in a state called current, this state will be used for editing a currently selected contact. added a reset to null value on delete function call

--- client/src/context/contact/ContactState.jsx
+++ client/src/context/contact/ContactState.jsx
@@ -37,6 +37,7 @@ const ContactState = (props) => {
         type: 'personal',
       },
     ],
+    current: null,
   };
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
@@ -50,9 +51,13 @@ const ContactState = (props) => {
     dispatch({ type: DELETE_CONTACT, payload: id });
   };
   // Set current contact
-
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
   // Clear current contact
-
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
   // Update contact
 
   // Filter contacts
@@ -63,8 +68,11 @@ const ContactState = (props) => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
         addContact,
         deleteContact,
+        setCurrent,
+        clearCurrent,
       }}>
       {props.children}
     </ContactContext.Provider>
