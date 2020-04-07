2199da0a1f5331e2c8481fd3f97efcb6c9e38288
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-07 18:22:26 -0400

    created login and registration components, brought in navbar links to new auth components

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,9 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import ContactState from './context/contact/ContactState';
-import AuthState from './context/contact/AuthState';
+import AuthState from './context/auth/AuthState';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 const App = () => {
   return (
@@ -18,6 +20,8 @@ const App = () => {
               <Switch>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
               </Switch>
             </div>
           </Fragment>
