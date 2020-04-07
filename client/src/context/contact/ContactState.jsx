b506396e43befd7b9e3bc7d09d601a2ddaada8cd
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-06 23:00:34 -0400

    added delete contact functions, verified works on static contact entries

--- client/src/context/contact/ContactState.jsx
+++ client/src/context/contact/ContactState.jsx
@@ -46,7 +46,9 @@ const ContactState = (props) => {
     dispatch({ type: ADD_CONTACT, payload: contact });
   };
   // Delete contact
-
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
   // Set current contact
 
   // Clear current contact
@@ -62,6 +64,7 @@ const ContactState = (props) => {
       value={{
         contacts: state.contacts,
         addContact,
+        deleteContact,
       }}>
       {props.children}
     </ContactContext.Provider>
