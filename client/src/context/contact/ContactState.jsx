10f0bf6a8afef66162c5ef5add825b9e43e9f340
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-08 16:24:26 -0400

    added get request for contacts, brought in spinner compnent, set contacts array to null on logout

--- client/src/context/contact/ContactState.jsx
+++ client/src/context/contact/ContactState.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
+  GET_CONTACTS,
+  CLEAR_CONTACTS,
   ADD_CONTACT,
   DELETE_CONTACT,
   SET_CURRENT,
@@ -15,12 +17,22 @@ import {
 
 const ContactState = (props) => {
   const initialState = {
-    contacts: [],
+    contacts: null,
     current: null,
     filtered: null,
     error: null,
   };
   const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Get Contacts
+  const getContacts = async () => {
+    try {
+      const res = await axios.get('/api/contacts');
+      dispatch({ type: GET_CONTACTS, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
+  };
 
   // Add Contact
   const addContact = async (contact) => {
@@ -41,6 +53,11 @@ const ContactState = (props) => {
   const deleteContact = (id) => {
     dispatch({ type: DELETE_CONTACT, payload: id });
   };
+  // Clear contacts from state
+  const clearContacts = () => {
+    dispatch({ type: CLEAR_CONTACTS });
+  };
+
   // Set current contact
   const setCurrent = (contact) => {
     dispatch({ type: SET_CURRENT, payload: contact });
@@ -68,11 +85,13 @@ const ContactState = (props) => {
         current: state.current,
         filtered: state.filtered,
         error: state.error,
+        getContacts,
         addContact,
         updateContact,
         deleteContact,
         setCurrent,
         clearCurrent,
+        clearContacts,
         filterContacts,
         clearFilter,
       }}>
--- client/src/context/contact/contactReducer.jsx
+++ client/src/context/contact/contactReducer.jsx
@@ -5,12 +5,21 @@ import {
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
+  CLEAR_CONTACTS,
   CLEAR_FILTER,
   CONTACT_ERROR,
+  GET_CONTACTS,
 } from '../types';
 
 export default (state, action) => {
   switch (action.type) {
+    case GET_CONTACTS:
+      return {
+        ...state,
+        contacts: action.payload,
+        loading: false,
+      };
+
     case ADD_CONTACT:
       return {
         ...state,
@@ -23,6 +32,7 @@ export default (state, action) => {
         contacts: state.contacts.map((contact) =>
           contact.id === action.payload.id ? action.payload : contact
         ),
+        loading: false,
       };
     case DELETE_CONTACT:
       return {
@@ -30,16 +40,19 @@ export default (state, action) => {
         contacts: state.contacts.filter(
           (contact) => contact.id !== action.payload
         ),
+        loading: false,
       };
     case SET_CURRENT:
       return {
         ...state,
         current: action.payload,
+        loading: false,
       };
     case CLEAR_CURRENT:
       return {
         ...state,
         current: null,
+        loading: false,
       };
     case FILTER_CONTACTS:
       return {
@@ -48,16 +61,27 @@ export default (state, action) => {
           const regex = new RegExp(`${action.payload}`, 'gi');
           return contact.name.match(regex) || contact.email.match(regex);
         }),
+        loading: false,
       };
     case CLEAR_FILTER:
       return {
         ...state,
         filtered: null,
+        loading: false,
       };
     case CONTACT_ERROR:
       return {
         ...state,
         error: action.payload,
+        loading: false,
+      };
+    case CLEAR_CONTACTS:
+      return {
+        ...state,
+        contacts: null,
+        filterd: null,
+        error: null,
+        current: null,
       };
 
     default:
