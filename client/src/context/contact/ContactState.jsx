026ca4440b8ca4e8f9cefa5de073ff2aadd4e4a1
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-08 19:29:41 -0400

    brought in update contact put request to MongoDB, verified functionality

--- client/src/context/contact/ContactState.jsx
+++ client/src/context/contact/ContactState.jsx
@@ -41,7 +41,6 @@ const ContactState = (props) => {
         'Content-Type': 'application/json',
       },
     };
-
     try {
       const res = await axios.post('/api/contacts', contact, config);
       dispatch({ type: ADD_CONTACT, payload: res.data });
@@ -59,6 +58,7 @@ const ContactState = (props) => {
       dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
     }
   };
+
   // Clear contacts from state
   const clearContacts = () => {
     dispatch({ type: CLEAR_CONTACTS });
@@ -68,18 +68,37 @@ const ContactState = (props) => {
   const setCurrent = (contact) => {
     dispatch({ type: SET_CURRENT, payload: contact });
   };
+
   // Clear current contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
   };
+
   // Update contact
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
   };
+
   // Filter contacts
   const filterContacts = (text) => {
     dispatch({ type: FILTER_CONTACTS, payload: text });
   };
+
   // Clear filter
   const clearFilter = () => {
     dispatch({ type: CLEAR_FILTER });
